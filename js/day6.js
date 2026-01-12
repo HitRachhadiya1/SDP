3b446bc5c2032dbc5d97558404999c9ef89b1f0f
Hit Rachhadiya <hitrachhadiya@gmail.com>
2026-01-12 22:31:19 +0530

    basics of javascript

---
+++ js/day6.js
@@ -0,0 +1,42 @@
+// console.log("Simple log ");
+// console.warn("test warning");
+// console.error("Test error");
+
+// console.log(myVar); // Output: undefined (No error, but no value)
+
+// var myVar = 10;
+
+// console.log(myVar); // Output: 10
+
+// console.log(myLet); // ❌ ReferenceError: Cannot access 'myLet' before initialization
+
+// let myLet = 20;
+
+// sayHello(); // Output: "Hello!" (Works perfectly)
+
+// function sayHello() {
+//   console.log("Hello!");
+// }
+
+// pass by value
+
+// let a = 10;
+// let b = a; // b is now 10 (a copy)
+
+// b = 20; // Changing b...
+// console.log(a); // Output: 10 (a stays the same!)
+
+// pass by referance
+
+// let number1 = { number: 10 };
+// let number2 = number1; // number2
+// // // points to the SAME object as number1
+
+// number2.name = 20; // We change number2
+// //...
+
+// console.log(number1.name); // Output:  (number1 CHANGED too!)
+
+console.log(typeof {});
+console.log(typeof []);
+console.log(typeof null);
